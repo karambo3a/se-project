6269e3864159913b68a63a51a2146bfd9cb08baf
mediana105 <st087217@student.spbu.ru>
2024-10-28 12:40:31 +0300

    Created User Entity

---
+++ server/src/main/java/org/example/User.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class User {
+    public static class Role {
+        public static final String STUDENT = "STUDENT";
+        public static final String TEACHER = "TEACHER";
+    }
+
+    private final String firstName;
+    private final String lastName;
+    private final String password;
+    private final String role;
+
+    public User(String firstName, String lastName, String password, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
