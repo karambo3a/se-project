bd9c531bf427e765b37f0c438d93cee7ae3facbb
mediana105 <st087217@student.spbu.ru>
2024-10-28 13:09:38 +0300

    Added unit test for basic scenarious of creating assignments and feedbcak

--- server/src/test/java/org/example/Test.java
+++ server/src/test/java/org/example/Test.java
@@ -6,8 +6,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class UserServiceTest {
@@ -34,7 +37,7 @@ class UserServiceTest {
 
     @Test
     void registerExistingTeacher() {
-        User newUser = new User("Petr", "Petrov", "password2", User.TEACHER);
+        User newUser = new User("Petr", "Petrov", "password2", User.Role.TEACHER);
         when(userRepository.existsByUsername("Petr", "Petrov")).thenReturn(false);
         boolean result = userService.registerNewUser(newUser);
         assertTrue(result);
@@ -48,4 +51,37 @@ class UserServiceTest {
         });
     }
 
+    @Test
+    void createTest() {
+        Long id = 1L;
+        Long assignmentId = 1L;
+        String feedbackText = "Excellent!";
+        LocalDateTime createdAt = LocalDateTime.now();
+        Feedback feedback = new Feedback(id, assignmentId, feedbackText, createdAt);
+        assertEquals(id, feedback.id());
+        assertEquals(assignmentId, feedback.assignmentId());
+        assertEquals(feedbackText, feedback.feedbackText());
+        assertEquals(createdAt, feedback.createdAt());
+    }
+
+    @Test
+    void createAssignment() {
+        Long id = 1L;
+        String title = "Homework 1";
+        LocalDateTime createdAt = LocalDateTime.now();
+        LocalDateTime dueDate = createdAt.plusDays(7);
+        Long studentId = 1L;
+        Long teacherId = 2L;
+        List<Assignment.PDF> files = Collections.emptyList();
+        Feedback feedback = new Feedback(1L, id, "Initial feedback", LocalDateTime.now());
+        Assignment assignment = new Assignment(id, title, createdAt, dueDate, studentId, teacherId, files, feedback);
+        assertEquals(id, assignment.getId());
+        assertEquals(title, assignment.getTitle());
+        assertEquals(createdAt, assignment.getCreatedAt());
+        assertEquals(dueDate, assignment.getDueDate());
+        assertEquals(studentId, assignment.getStudentId());
+        assertEquals(teacherId, assignment.getTeacherId());
+        assertEquals(files, assignment.getFiles());
+        assertEquals(feedback, assignment.getFeedback());
+    }
 }
