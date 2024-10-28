ff94bef65cd8494c68483e6b1d308d34e649ce7a
mediana105 <st087217@student.spbu.ru>
2024-10-28 12:41:37 +0300

    Added unit tests for basic scenarios of registration

--- server/src/test/java/org/example/Test.java
+++ server/src/test/java/org/example/Test.java
@@ -1,9 +1,14 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 class UserServiceTest {
 
@@ -17,4 +22,30 @@ class UserServiceTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
     }
+
+    @Test
+    void registerExistingStudent() {
+        User newStudent = new User("Ivan", "Ivanov", "password1", User.Role.STUDENT);
+        when(userRepository.existsByUsername("Ivan", "Ivanov")).thenReturn(false);
+        boolean result = userService.registerNewUser(newStudent);
+        assertTrue(result);
+        verify(userRepository, times(1)).save(newStudent);
+    }
+
+    @Test
+    void registerExistingTeacher() {
+        User newUser = new User("Petr", "Petrov", "password2", User.TEACHER);
+        when(userRepository.existsByUsername("Petr", "Petrov")).thenReturn(false);
+        boolean result = userService.registerNewUser(newUser);
+        assertTrue(result);
+        verify(userRepository, times(1)).save(newUser);
+    }
+
+    @Test
+    void registerNullUser() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            userService.registerNewUser(null);
+        });
+    }
+
 }
