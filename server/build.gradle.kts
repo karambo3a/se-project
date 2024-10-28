9acdbb76a5803f781ec09bd7c08b1f801077d160
mediana105 <st087217@student.spbu.ru>
2024-10-28 14:00:23 +0300

    Added dependencies for server

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.0"
     id("java")
 }
 
@@ -10,10 +12,13 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.h2database:h2")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.mockito:mockito-core:5.4.0")
-
 }
 
 tasks.withType<JavaCompile> {
